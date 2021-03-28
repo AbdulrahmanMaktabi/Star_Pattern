5db5379e32cb6cc98f5a64e142b0a70d9c96f0da
abdurlrahman <abdurahmanmaktabi@gmail.com>
2021-03-28 18:44:28 +0300

    Program 1

---
+++ program1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    do {
+        cout << "Enter the number of lines: ";
+        cin >> n;
+    } while (n < 0 || n % 2 == 0);
+
+    for (int i = 1; i <= n; i++) {
+        if (i <= n / 2 + 1) {
+            for (int j = 1; j <= (n / 2)+ 1 - i; j++) {
+                cout << " ";
+            }
+            for (int k = 1; k <= (i * 2) - 1; k++) {
+                cout << "*";
+            }
+        }
+        else {
+            for (int j = 1; j <= i - ((n /2) +1); j++) {
+                cout <<" ";
+            }
+            for (int k = 1; k < ((n*2) - (i*2) + 2); k++) {
+                cout <<"*";
+            }
+        }
+        cout<< endl;
+    }
+    return 0;
+}
